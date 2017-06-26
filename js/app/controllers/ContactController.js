c37c713af2b945a09610cdfc4d2b6895227b93cb
Mdjurdj1 <mdjurdj1@gmail.com>
2017-06-26 00:01:09 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
-function ContactController($scope) {
+function ContactController($scope, $timeout) {
 	$scope.name = 'Bill Gates';
+	$timeout(function () {
+		$scope.name = 'Mike Djurdjevic'
+	}, 5000)
 }
 
 angular
