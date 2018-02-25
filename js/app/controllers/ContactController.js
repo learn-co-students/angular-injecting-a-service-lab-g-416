e4696d2db458d31619987d8e21e5bd77d7022bd5
github username <12ivazquez@gmail.com>
2018-02-24 18:49:42 -0600

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Isabel';
+
+	$timeout(function () {
+  	$scope.name = 'Anna';
+	}, 5000);
 }
 
 angular
