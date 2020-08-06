d91168749d3d9232201891cecc7d2d572c0d461c
Paul Rosero <paul_rosero@yahoo.com>
2020-08-06 16:34:13 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -2,6 +2,10 @@ function ContactController($scope) {
 	$scope.name = 'Bill Gates';
 }
 
+$timeout(function () {
+	// this would be fired after 2 seconds!
+}, 5000);
+
 angular
 	.module('app')
 	.controller('ContactController', ContactController);
