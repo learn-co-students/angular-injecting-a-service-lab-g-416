757fcde763ebba4e0114433820caf71363508454
kikuishimoto <kikuish@gmail.com>
2020-10-28 11:47:25 -0700

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,10 +1,10 @@
-function ContactController($scope, $timeout) {
-	$scope.name = 'Bill Gates';
-	$timeout(function(){
-		$scope.name ="kiku"
+function ContactController(j,k) {
+	j.name = 'Bill Gates';
+	k(function(){
+		j.name ="kiku"
 	}, 5000)
 }
-
+ContactController.$inject = ['$scope', '$timeout'];
 angular
 	.module('app')
 	.controller('ContactController', ContactController);
